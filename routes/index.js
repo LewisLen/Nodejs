ca79e1cdb91856e2374981602d71180fd6c0fa54
LewisLen <csshack2016@126.com>
2022-04-09 08:03:22 +0800

    fix: 修复路由加载顺序

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ const usersRouter = require("./users");
 const moviesRouter = require("./movie");
 
 module.exports = (app) => {
+  app.use("/users", usersRouter);
+  app.use("/movies", moviesRouter);
   app.use("/", (req, res, next) => {
     res.json({ title: "Express" });
   });
-  app.use("/users", usersRouter);
-  app.use("/movies", moviesRouter);
 };
